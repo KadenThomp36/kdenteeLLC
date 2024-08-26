802a68de8779f1d86659f2484d4916425cad178d
KadenThomp36 <kadenthomp36@gmail.com>
2024-08-26 19:43:58 -0400

    created main pages and their modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainpageComponent } from '@layout/mainpage/mainpage.component';
+import { AboutModule } from '@modules/about/about.module';
+import { HomeModule } from '@modules/home/home.module';
+import { ServicesModule } from '@modules/services/services.module';
 
 const routes: Routes = [
   {
     path: '',
     component: MainpageComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('@modules/home/home.module').then(
+            (m): typeof HomeModule => m.HomeModule
+          ),
+      },
+      {
+        path: 'about',
+        loadChildren: () =>
+          import('@modules/about/about.module').then(
+            (m): typeof AboutModule => m.AboutModule
+          ),
+      },
+      {
+        path: 'services',
+        loadChildren: () =>
+          import('@modules/services/services.module').then(
+            (m): typeof ServicesModule => m.ServicesModule
+          ),
+      },
+    ],
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from '@shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,7 +20,13 @@ import { BrowserModule } from '@angular/platform-browser';
     AppRoutingModule,
     CoreModule,
     SharedModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
+  providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
