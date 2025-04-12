cba774620e82a8f52194a945a6469e215396ef80
KadenThomp36 <kadenthomp36@gmail.com>
2025-04-12 16:34:07 -0400

    straight to prod

--- src/app/data/services/utilities/core-utilities.service.ts
+++ src/app/data/services/utilities/core-utilities.service.ts
@@ -10,9 +10,29 @@ export class CoreUtilitiesService {
   routeTo(path: string) {
     this.router.navigate([path]);
   }
+
   trackNavigation(label: string) {
     gtag('event', 'navigation_click', {
       event_category: 'Navigation',
+      event_label: label,
+    });
+  }
+
+  trackFormSuccess(label: string) {
+    gtag('event', 'form_submission', {
+      event_category: 'Contact',
+      event_label: label,
+    });
+  }
+  trackFormError(label: string) {
+    gtag('event', 'form_submission_error', {
+      event_category: 'Contact',
+      event_label: label,
+    });
+  }
+  trackEvent(eventName: string, category: string, label: string) {
+    gtag('event', eventName, {
+      event_category: category,
       event_label: label,
     });
   }
